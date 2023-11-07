6cf84f2f82574d0f8914de52b90838ebf1e9584c
silverZZ0124 <72031018+silverZZ0124@users.noreply.github.com>
2023-11-07 16:09:43 +0900

    [Feat] - 로고, scss 추가

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import Complete from "./sign-up/pages/complete/Complete";
+import Interests from "./sign-up/pages/interests/Interests";
 import RegForm from "./sign-up/pages/reg-from/RegForm";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/signup/regform" element={<RegForm />} />
+      <Route path="/signup/interests" element={<Interests />} />
+      <Route path="/signup/complete" element={<Complete />} />
     </Routes>
   );
 }
