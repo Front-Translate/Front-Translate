8031a2efc47426eb824b5e215fab0deaaaf13912
s11verz <72031018+s11verz@users.noreply.github.com>
2023-11-07 16:00:19 +0900

    [Feat] - sign up/regform,interests,complete 페이지 기능 구현 완료

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import RegForm from "./sign-up/pages/reg-from/RegForm";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<div>라우터</div>} />
+      <Route path="/signup/regform" element={<RegForm />} />
     </Routes>
   );
 }
