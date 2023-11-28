ebfea251c74b9e591150be534dabdb586da54340
s11verz <72031018+s11verz@users.noreply.github.com>
2023-11-28 18:20:30 +0900

    [Feat] - main slider 구현 진행중

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,17 @@
 import styles from "./Main.module.scss";
-import ContentSlider from "./component/slider/Slider";
+import ContentSlider from "./component/contentSlider/ContentSlider";
+import HeaderSlider from "./component/headerSlider/HeaderSlider";
 
 const Main = () => {
   return (
     <>
       <div className={styles.container}>
+        <div className={styles.headerSection}>
+          <HeaderSlider />
+        </div>
         <div className={styles.content}>
-          <div className={styles.headerSection}>
+          <div className={styles.divider}></div>
+          <div className={styles.bodySection}>
             <ContentSlider />
           </div>
         </div>
